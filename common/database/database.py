979c06557f0d2368e700b2485a1783ea12ed0c6b
Samiya <vemodalen@163.com>
2023-11-27 14:53:20 +0800

    重写数据库操作

--- common/database/database.py
+++ common/database/database.py
@@ -1,153 +1,239 @@
 import sqlite3
-from typing import Tuple
+from typing import Tuple, List, Optional
 
 from loguru import logger
 
 
 class ProductDatabase:
-    # 将 SQL 查询语句作为类属性
-    SQL_CREATE_WEBSITE_KEYWORDS_TABLE = """
-        CREATE TABLE IF NOT EXISTS website_keywords (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            website TEXT NOT NULL,
-            keyword TEXT NOT NULL,
-            product_count INTEGER DEFAULT 0,
-            UNIQUE(website, keyword)
-        );
-    """
-    SQL_CREATE_PRODUCTS_TABLE = """
-        CREATE TABLE IF NOT EXISTS products (
-            id TEXT NOT NULL,
-            keyword_id INTEGER NOT NULL,
-            name TEXT,
-            price REAL NOT NULL,
-            image_url TEXT,
-            product_url TEXT,
-            PRIMARY KEY (id, keyword_id),
-            FOREIGN KEY (keyword_id) REFERENCES website_keywords (id)
-        );
-    """
-    SQL_UPSERT_QUERY = """
-    INSERT INTO products (id, keyword_id, price, name, image_url, product_url) 
-    VALUES (?, ?, ?, ?, ?, ?)
-    ON CONFLICT(id, keyword_id) DO UPDATE SET
-    price = excluded.price,
-    name = excluded.name,
-    image_url = excluded.image_url,
-    product_url = excluded.product_url;
-    """
+    # SQL 语句集中管理
+    SQL_STATEMENTS = {
+        "create_website_keywords_table": """
+            -- 创建一个用于存储网站关键词的表
+            -- 如果表已存在，则此操作不会有任何效果
+            CREATE TABLE IF NOT EXISTS website_keywords (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,  -- 主键，自动递增
+                website TEXT NOT NULL,                 -- 网站名
+                keyword TEXT NOT NULL,                 -- 关键词
+                product_count INTEGER DEFAULT 0,       -- 产品计数，默认为 0
+                UNIQUE(website, keyword)               -- 确保每个网站和关键词的组合是唯一的
+            );
+        """,
+        "create_products_table": """
+            -- 创建一个用于存储产品信息的表
+            -- 如果表已存在，则此操作不会有任何效果
+            CREATE TABLE IF NOT EXISTS products (
+                id TEXT NOT NULL,                      -- 产品 ID
+                keyword_id INTEGER NOT NULL,           -- 关联的关键词 ID
+                name TEXT,                             -- 产品名
+                price REAL NOT NULL,                   -- 价格
+                image_url TEXT,                        -- 图片 URL
+                product_url TEXT,                      -- 产品 URL
+                PRIMARY KEY (id, keyword_id),          -- 将产品 ID 和关键词 ID 一起作为主键
+                FOREIGN KEY (keyword_id) REFERENCES website_keywords (id) -- 外键关联到 website_keywords 表
+            );
+        """,
+        "upsert_product": """
+            -- 插入或更新产品信息
+            -- 如果具有相同的产品 ID 和关键词 ID 的记录已存在，则更新该记录
+            INSERT INTO products (id, keyword_id, price, name, image_url, product_url) 
+            VALUES (?, ?, ?, ?, ?, ?)
+            ON CONFLICT(id, keyword_id) DO UPDATE SET
+            price = excluded.price,
+            name = excluded.name,
+            image_url = excluded.image_url,
+            product_url = excluded.product_url;
+        """,
+        "insert_or_ignore_keyword": """
+            -- 插入新的网站和关键词对
+            -- 如果相同的网站和关键词对已存在，则忽略此插入
+            INSERT OR IGNORE INTO website_keywords (website, keyword) VALUES (?, ?);
+        """,
+        "select_keyword_id": """
+            -- 根据网站名和关键词选择关键词 ID
+            SELECT id FROM website_keywords WHERE website = ? AND keyword = ?;
+        """,
+        "update_product_count": """
+            -- 更新特定关键词 ID 的产品计数
+            UPDATE website_keywords SET product_count = ? WHERE id = ?;
+        """,
+        "count_products_by_keyword": """
+            -- 计算特定关键词的产品总数
+            SELECT COUNT(*) FROM products WHERE keyword_id = ?;
+        """,
+        "bulk_fetch_prices": """
+            -- 批量获取一系列产品的价格
+            -- 使用参数替换来构建查询的 IN 子句
+            SELECT id, price FROM products WHERE (id, keyword_id) IN ({});
+        """,
+    }
 
     def __init__(self, db_name: str):
+        """
+        初始化 ProductDatabase 实例。
+
+        :param db_name: 数据库的文件名。
+        """
         self.db_name = db_name
         self.conn = sqlite3.connect(self.db_name, check_same_thread=False)
         self.setup_database()
 
-    def upsert_products(self, items, keyword, website):
+    def setup_database(self):
+        """创建数据库表格，如果它们不存在的话。"""
+        self._safe_execute("create_website_keywords_table")
+        self._safe_execute("create_products_table")
+
+    def _safe_execute(
+        self, query_key: str, params: Tuple = (), fetch_one=False, fetch_all=False
+    ):
+        """
+        安全地执行数据库查询，并处理任何数据库异常。
+
+        :param query_key: SQL_STATEMENTS 字典中的键，用于指定要执行的 SQL 语句。
+        :param params: 传递给 SQL 语句的参数。
+        :param fetch_one: 如果为 True，则只返回查询的第一行。
+        :param fetch_all: 如果为 True，则返回查询的所有行。
+        :return: 根据 fetch_one 和 fetch_all 返回相应的查询结果，如果出现异常，则返回 None。
+        """
+        query = self.SQL_STATEMENTS[query_key]
+        try:
+            cursor = self.conn.execute(query, params)
+            if fetch_one:
+                return cursor.fetchone()
+            if fetch_all:
+                return cursor.fetchall()
+            return cursor
+        except sqlite3.DatabaseError as e:
+            logger.error(f"Database error: {e}")
+            return None
+
+    def insert_or_ignore_keyword(self, website: str, keyword: str):
+        """
+        插入一个新的 website 和 keyword 对，如果它们已存在，则忽略此操作。
+
+        :param website: 网站名。
+        :param keyword: 关键词。
+        """
+        self._safe_execute("insert_or_ignore_keyword", (website, keyword))
+
+    def get_keyword_id(self, website: str, keyword: str) -> Optional[int]:
+        """
+        根据给定的网站名和关键词获取关键词 ID。
+
+        :param website: 网站名。
+        :param keyword: 关键词。
+        :return: 对应的关键词 ID，如果未找到则返回 None。
+        """
+        result = self._safe_execute(
+            "select_keyword_id", (website, keyword), fetch_one=True
+        )
+        return result[0] if result else None
+
+    def update_product_count(self, keyword_id: int):
+        """
+        更新特定关键词 ID 的产品数量。
+
+        :param keyword_id: 关键词 ID。
+        """
+
+        count = self._safe_execute(
+            "count_products_by_keyword", (keyword_id,), fetch_one=True
+        )[0]
+        self._safe_execute("update_product_count", (count, keyword_id))
+
+    def upsert_products(self, items: List[dict], keyword: str, website: str):
+        """
+        插入或更新产品信息。
+
+        :param items: 包含产品信息的字典列表。
+        :param keyword: 关联的关键词。
+        :param website: 关联的网站。
+        :yield: 处理后的每个产品信息。
+        """
         logger.info(
             f"Number of items entered into the database for processing: {len(items)}"
         )
-
         self.insert_or_ignore_keyword(website, keyword)
         keyword_id = self.get_keyword_id(website, keyword)
+        existing_prices = self._bulk_fetch_prices(items, keyword_id)
 
         to_insert_or_update = []
-
         updated_num = 0
         new_num = 0
 
         for item in items:
-            item_key = (item["id"], keyword_id)
             current_price = item["price"]
+            existing_price = existing_prices.get(item["id"])
 
-            # 仅当需要时查询现有价格
-            existing_price = self._execute_query(
-                "SELECT price FROM products WHERE id = ? AND keyword_id = ?;", item_key
-            ).fetchone()
-
-            # 判断是否需要插入或更新
-            if existing_price is not None:
-                if current_price != existing_price[0]:
-                    # 价格有变动，准备更新数据
-                    item["price_change"] = 2 if current_price > existing_price[0] else 3
-                    to_insert_or_update.append(
-                        (
-                            item["id"],
-                            keyword_id,
-                            item["price"],
-                            item["name"],
-                            item["image_url"],
-                            item["product_url"],
-                        )
-                    )
-                    item["pre_price"] = existing_price[0]
-                    updated_num += 1
-                    yield item
-            else:
-                to_insert_or_update.append(
-                    (
-                        item["id"],
-                        keyword_id,
-                        item["price"],
-                        item["name"],
-                        item["image_url"],
-                        item["product_url"],
-                    )
-                )
+            if existing_price is None:
+                # 商品在数据库中不存在，视为新商品
                 item["price_change"] = 1
                 new_num += 1
-                yield item
+            elif current_price != existing_price:
+                # 商品价格变化了，视为更新的商品
+                item["price_change"] = 2 if current_price > existing_price else 3
+                item["pre_price"] = existing_price
+                updated_num += 1
+            else:
+                # 商品存在且价格未变，跳过处理
+                continue
+
+            to_insert_or_update.append(
+                (
+                    item["id"],
+                    keyword_id,
+                    item["price"],
+                    item["name"],
+                    item["image_url"],
+                    item["product_url"],
+                )
+            )
+            yield item
 
         if to_insert_or_update:
             with self.conn:  # 使用事务处理
-                self.conn.executemany(self.SQL_UPSERT_QUERY, to_insert_or_update)
+                self.conn.executemany(
+                    self.SQL_STATEMENTS["upsert_product"], to_insert_or_update
+                )
 
         self.update_product_count(keyword_id)
         if updated_num > 0 or new_num > 0:
-            logger.info(
-                f"Number of updated items updated in the database, Updated: {updated_num}/New: {new_num}"
-            )
+            logger.info(f"Database updated. New: {new_num}, Updated: {updated_num}")
 
-    def _execute_query(self, query: str, params: Tuple = ()):
-        """执行 SQL 查询"""
-        try:
-            cursor = self.conn.execute(query, params)
-            return cursor
-        except sqlite3.DatabaseError as e:
-            logger.error(f"Database error: {e}")
-            # 根据需要添加更多的错误处理逻辑
-            return None
+    def _bulk_fetch_prices(self, items: List[dict], keyword_id: int) -> dict:
+        """
+        批量获取产品的当前价格。
 
-    def setup_database(self):
-        with self.conn:
-            self._execute_query(self.SQL_CREATE_WEBSITE_KEYWORDS_TABLE)
-        with self.conn:
-            self._execute_query(self.SQL_CREATE_PRODUCTS_TABLE)
+        :param items: 包含产品信息的字典列表。
+        :param keyword_id: 关联的关键词 ID。
+        :return: 一个字典，包含产品 ID 和对应的价格。
+        """
 
-    def insert_or_ignore_keyword(self, website: str, keyword: str):
-        with self.conn:  # 这将自动处理事务
-            self._execute_query(
-                "INSERT OR IGNORE INTO website_keywords (website, keyword) VALUES (?, ?);",
-                (website, keyword),
-            )
+        # 构造一个包含所有项目 ID 和关键字 ID 的元组列表
+        ids = [(item["id"], keyword_id) for item in items]
 
-    def get_keyword_id(self, website: str, keyword: str) -> int:
-        cursor = self._execute_query(
-            "SELECT id FROM website_keywords WHERE website = ? AND keyword = ?;",
-            (website, keyword),
+        # 为每个 (id, keyword_id) 对创建占位符 "?"
+        placeholders = ",".join(["(?, ?)" for _ in ids])
+
+        # 构造查询字符串
+        query = "SELECT id, price FROM products WHERE (id, keyword_id) IN ({})".format(
+            placeholders
         )
-        return cursor.fetchone()[0]
+        # query = self.SQL_STATEMENTS["bulk_fetch_prices"].format(placeholders)
+        # 将元组列表展开为参数列表
+        params = [param for tup in ids for param in tup]
 
-    def update_product_count(self, keyword_id):
-        with self.conn:
-            count = self._execute_query(
-                "SELECT COUNT(*) FROM products WHERE keyword_id = ?;", (keyword_id,)
-            ).fetchone()[0]
-            self._execute_query(
-                "UPDATE website_keywords SET product_count = ? WHERE id = ?;",
-                (count, keyword_id),
-            )
+        # 执行查询
+        cursor = self.conn.execute(query, params)
+        result = cursor.fetchall()
+
+        # 构造并返回包含查询结果的字典
+        # 只包含数据库中实际存在的产品的价格
+        prices = {id: price for id, price in result}
+        return prices
 
     def close(self):
-        """显式关闭数据库连接"""
+        """
+        显式关闭数据库连接。
+        """
         self.conn.close()
         logger.info(f"Database connection closed: {self.db_name}")
